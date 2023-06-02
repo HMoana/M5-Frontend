1692f0057ab82b3ee70677df2a86c70ad726b54a
Heni Kimura <henace86@gmail.com>
2023-06-02 15:34:52 +1200

    trying to connecting mongodb to frontend, still wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+  const [helloWorld, setHelloWorld] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8081/helloWorld")
+      .then((response) => {
+        // handle success
+        setHelloWorld(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  }, []);
+
+  return <div>{helloWorld}</div>;
 }
 
 export default App;

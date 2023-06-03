9b4ff9b1a6b9347d3d71089dc41a5a5e3fd041cc
Jin Zhang <jinzh235@gmail.com>
2023-06-03 15:17:57 +1200

    first commit 030623 3:17pm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import React from "react";
+import Header from "./Components/CommonComponents/Header";
+import Footer from "./Components/CommonComponents/Footer";
+import HeaderImageContainer from "./Components/Homepage/HeaderImageContainer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <Header />
+      <HeaderImageContainer />
+      <Footer />
     </div>
   );
 }

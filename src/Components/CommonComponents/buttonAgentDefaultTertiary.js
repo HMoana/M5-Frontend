c51bc1c43d43bde2f482def057c18b569f2cbe27
hmoana <henace555@live.com>
2023-06-09 04:21:52 +1200

    completed listingInfo page on feature branch. wont merge to master until tomorrow when with Jin & ricardo

---
+++ src/Components/CommonComponents/buttonAgentDefaultTertiary.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./buttonAgentDefaultTertiary.module.css";
+
+const buttonAgentDefaultTertiary = () => {
+  return (
+    <div>
+      <button className={styles.dAgentBtns}>Enquire</button>
+    </div>
+  )
+}
+
+export default buttonAgentDefaultTertiary
--- src/Components/ListingInfoPage/ListingInfoPage.js
+++ src/Components/ListingInfoPage/ListingInfoPage.js
@@ -1,10 +1,16 @@
 import Header from "../CommonComponents/Header";
 import Footer from "../CommonComponents/Footer";
+import ListingInfoGallery from "./ListingInfoGallery";
+import ListingInfoMainContainer from "./ListingInfoMainContainer";
+import ListingInfoMap from "./ListingInfoMap";
 
 const ListingInfoPage = () => {
   return (
     <div>
       <Header />
+      <ListingInfoGallery />
+      <ListingInfoMainContainer />
+      <ListingInfoMap />
       <Footer />
     </div>
   );
